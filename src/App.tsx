72d3a9ca374fc7bcd2ffb5974fa05e92b8a542db
NeonNature <mustachemaung@gmail.com>
2023-10-04 10:20:20 +0630

    chore: init commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <p>Yangon, Myanmar</p>
+        <p>Some image goes here</p>
+        <p>64 C</p>
+        <p>Clear</p>
+      </div>
+      <hr />
+      <div>
+        <p>Mon</p>
+        <p>Some image goes here</p>
+        <p>22/16</p>
+      </div>
+    </>
+  )
+}
+
+export default App
